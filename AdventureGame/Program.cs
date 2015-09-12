5b1f55bf0411917dc26858b61524f476cba43be5
Joe Cormier <jncormier24@gmail.com>
2015-09-12 15:48:43 -0400

    Initial Commit - Setup basic structure for classes and methods.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(  );
+            game.Playing = true;
+            while (game.Playing)
+            {
+                Console.WriteLine("Please enter the name of your new Hero: ");
+                string Name = Console.ReadLine();
+
+                Hero Hero = new Hero();
+                Hero.Name = Name;
+            }
+        }
+    }
+
+    class Game {
+        public bool Playing
+        {
+            get; set;
+        }
+        public void GameOver() {
+            
+        }
+        public void Save() { }
+    }
+    class Character {
+        public string Name
+        {
+            get; set;
+        }
+        public string Type
+        {
+            get; set;
+        }
+        public int Health
+        {
+            get; set;
+        }
+    }
+    class Hero : Character {
+
+    }
+    class Enemy : Character {
+
+    }
+    class Item {
+        public string Name
+        {
+            get; set;
+        }
+    }
+    class Weapon : Item {
+        public int Damage {
+            get; set;
+        }
+        public int Durability
+        {
+            get; set;
+        }
+    }
+}
