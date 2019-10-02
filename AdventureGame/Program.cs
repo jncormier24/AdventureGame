d34e0d475b3228078820f962b2dbe8718648ebc2
Magnus Sundström <kmvsundstrom@gmail.com>
2019-10-02 08:05:51 +0200

    Fixed inconsistent variable and method casings

--- AdventureGame/Program.cs
+++ AdventureGame/Program.cs
@@ -7,24 +7,26 @@ namespace AdventureGame
     {
         static void Main(string[] args)
         {
-            Game Game = new Game(  );
-            Game.Playing = true;
-            while (Game.Playing)
+            var game = new Game();
+            game.Playing = true;
+            while (game.Playing)
             {
                 Console.WriteLine("Please enter the name of your new Hero: ");
                 string Name = Console.ReadLine();
 
-                Hero Hero = new Hero();
-                Hero.Name = Name;
+                var hero = new Hero();
+                hero.Name = Name;
 
-                Console.WriteLine("Your name is: "+Hero.Name);
+                Console.Clear();
+
+                Console.WriteLine($"Your name is {hero.Name}");
                 Console.WriteLine("What would you like to do: \n"
                                   +"\t 1.) Go into town\n"
                                   +"\t 2.) Go into the forest\n");
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        Game.goToTown( "Belmont" );
+                        game.GoToTown("Belmont");
                         break;
                     default:
                         Console.WriteLine("Thank you for playing the game");
